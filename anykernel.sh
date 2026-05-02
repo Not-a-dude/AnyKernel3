9536c9b24a1e434ed8236d2392658fd8144bc00d
Not-a-dude <honkhonkovic842@gmail.com>
2026-05-02 10:28:48 +0700

    anykernel: not systemless install

--- anykernel.sh
+++ anykernel.sh
@@ -7,7 +7,7 @@ properties() { '
 kernel.string=ExampleKernel by Not a Dude @ xda-developers
 do.devicecheck=1
 do.modules=1
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=miatoll
